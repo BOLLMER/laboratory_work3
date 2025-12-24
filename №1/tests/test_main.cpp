422450aab4d70032c58840e6012e6eafa5f5bc1a
BOLLMER <162329816+BOLLMER@users.noreply.github.com>
2025-12-24 13:49:44 +0700

    Тестовые файлы

---
+++ №1/tests/test_main.cpp
@@ -0,0 +1,16 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
